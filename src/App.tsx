60e6002f60941af61af591b2a484e5330edff1e9
sinnaclass <sinnahome@naver.com>
2025-07-10 14:50:28 +0900

    8.1 React Hooks 학습 - useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,55 @@
-import { Gift, Search, ShoopingBag, User } from "lucide-react";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="page">
-      {/*헤더 영역*/}
-      <header className="header"></header>
-      <nav className="nav"></nav>
-      <button>지금핫한</button>
-      <button>실시간</button>
-      <button>오늘신작</button>
-      <button>이벤트</button>
-      <button>낭성인기</button>
-      <button>완결추천</button>
-      <button>완전판</button>
-      <button>여성인기</button>
-      <button>TV속 작품</button>
-      <button>브랜드</button>
+  //useState => Hooks
+  // USTstAT는 가장기본이면 HOOK 이며, 함수컴포넌트에서도 가변적인 상태를 지닐수 있게 해준다
+  // 이 함수가 호출되면 배열을 반환한다. => 첫번째 요소는 상태 값, 두번째 요소는 상태는 설정하는 함수
 
-      {/*왼쪽 영역*/}
-      <div>
-        {/*텍스트 로고*/}
-        <div classNAME="header_left_logo">kakaopage</div>
+  const[test,setTest] =useState<string>(0);  
+  const [value, setValue] = useState(0); // => 이 함수가 호출되면 배열을 반환한다.  
+  const [name, setName] = useState(0); // => 이 함수가 호출되면 배열을 반환한다.
+  const [nickname, setNickname]
+//   //useState함수의 파라미터에는 상태의 기본값을 넣어줍니다
 
-        {/*메뉴 영역*/}
+// //   return (
+// //     <div>
+// //       <P>
+// //         {" "}
+// //         현재 카운터 값은<B>{alue}</B> 입니다
+// //       </P>
+
+// //       <button onClick={() => setValue(value + 1)}>+1</button>
+// //       <button onClick={() => setValue(value - 1)}>-1</button>
+// //     </div>
+// //   );
+// // }
+
+// export default App;
+
+  CONST onchange = e => {
+    setName(e.target.value);
+  };
+
+  CONST onchangeNickname = e => {
+    setNickname(e.target.value);0
+  };
+
+  return(
+    <div>
         <div>
-          <a href="" className="menu">
-            추천
-          </a>
-          <a href="" className="menu">
-            웹툰
-          </a>
-          <a href="" className="">
-            웹소설
-          </a>
-          <a href="" className="">
-            책
-          </a>
-          <a href="" className="">
-            바로가기
-          </a>
+            <input value={name} onChange = {onChangeName} />
+            <input value={nickname} onChange={onChangeNickname}/>
+            
         </div>
+    <div>
+      <div>
+        <b>이름:</b>{name}
+      </div>
+      <div>
+        <b>닉네임:</b>{nickname}
       </div>
 
-      {/*오른쪽 영역*/}
-      <div className="header_right">
-        {/*검색창 UI 로고*/}
-        <div className="header_right_search-box"></div>
-        <input type="text" placeholder="제목, 작가"></input>
-        <button class="name">
-          <ArrowRight />
-        </button>
-
-        {/*버튼 UI*/}
-        <div className="header_right_btn_box"></div>
-        <button className="header_ri"></button>
-        <button></button>
-        <button className="nav"></button>
-      </div>
     </div>
-  );
-}
 
-export default App;
+    </div>
+  
